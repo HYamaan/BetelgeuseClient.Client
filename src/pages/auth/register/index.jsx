4058d9054eb6f969a3eda260edddee16598f623d
Hakan Yaman <hakan.yaman@d-teknoloji.com.tr>
2024-01-07 10:54:35 +0300

    register page add module.css

--- src/pages/auth/register/index.jsx
+++ src/pages/auth/register/index.jsx
@@ -8,6 +8,7 @@ import {registerSchema} from "@/schema/registerSchema";
 import {FaRegEnvelope, FaUserAlt} from "react-icons/fa";
 import {BiSolidLockAlt} from "react-icons/bi";
 import {useFormik} from "formik";
+import styles from "@/pages/auth/authPage.module.css";
 
 const RegisterPage = () => {
     const onSubmit = async (values, actions) => {
@@ -72,18 +73,19 @@ const RegisterPage = () => {
         }
     ]
     return <>
-        <div className="container mx-auto flex items-center justify-center gap-6  mt-20">
-            <div className="flex-1 self-start">
+        <div className={styles.authContainer}>
+            <div className={styles.authContainer_Image}>
                 <Image
                     src={loginPic}
                     alt="Picture of the author"
-                    className="align-middle scale-110 "
+                    className={styles.authContainer_Image_Styles}
                 />
             </div>
-            <div className="flex-1">
-                <div className="flex flex-col">
-                    <p className="text-5xl font-semibold text-fogGray">Sign up<span className="text-sapphireBlue">!</span></p>
-                    <p className="mt-7 mb-10 text-lg text-fogGray">Explore, learn, and grow with us. enjoy a seamless and enriching educational journey. lets begin!</p>
+            <div className={styles.authContainer_Input_Boxes}>
+                <div className={styles.authContainer_Input_Boxes_Child}>
+                    <p className={styles.login_title}>Sign up<span className={styles.login_title_marker}>!</span></p>
+                    <p className={styles.login_description}>Explore, learn, and grow with us. enjoy a seamless and enriching educational journey. lets begin!</p>
+
                     <form onSubmit={RegisterFormik.handleSubmit}>
                         {RegisterInputInfo.map((input) => {
                             return <AuthActionButton
@@ -93,10 +95,13 @@ const RegisterPage = () => {
                                 onBlur={RegisterFormik.handleBlur}
                             />
                         })}
-                        <input type="submit" value="Login"  className="w-full bg-sapphireBlue text-xl rounded-2xl p-4 text-[#fff] cursor-pointer"/>
+                        <input type="submit" value="Login"  className={styles.button}/>
                     </form>
-                    <p className="text-right mt-2 text-lg cursor-pointer">Forgot password?</p>
-                    <p className="self-center text-2xl  mt-10">Already you have an account? <span className="m-5 text-sapphireBlue cursor-pointer">Log in</span></p>
+                    <p className={styles.forgot_password}>Forgot password?</p>
+                    <p className={styles.dont_have_account}>
+                        Already you have an account?
+                        <span className={styles.login_page_signup}>Log in</span>
+                    </p>
                 </div>
             </div>
         </div>
